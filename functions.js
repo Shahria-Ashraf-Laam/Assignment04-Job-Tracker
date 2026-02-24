2530283f658ffcaacf4d4bcadbff1f42c807c81b
Shahria-Ashraf-Laam <shahria.ashraf45@gmail.com>
2026-02-24 14:44:46 +0600

    Some JS functions completed and added to function.js file

--- functions.js
+++ functions.js
@@ -1,1 +1,36 @@
-console.log("wokring!")
+let rejectedList = [];
+let interviewList = [];
+let currentStatus = 'all'
+
+let total = document.getElementById('total-count');
+let interviewCount = document.getElementById('interview-count');
+let rejectedCount = document.getElementById('rejected-count');
+let countJobs = document.getElementById('count-jobs');
+
+const allFilterBtn = document.getElementById('all-filter-btn');
+const interviewFilterBtn = document.getElementById('interview-filter-btn');
+const rejectedFilterBtn = document.getElementById('rejected-filter-btn');
+const filteredSection = document.getElementById('filtered-section');
+
+const allCardSection = document.getElementById('all-cards');
+
+const mainContainer = document.querySelector('main');
+
+function calculateCount() {
+    const currentTotal = allCardSection.children.length;
+    total.innerText = currentTotal;
+
+    if (currentStatus === 'interview-filter-btn') {
+        countJobs.innerText = `${interviewList.length} of ${currentTotal}`;
+    }
+    else if (currentStatus === 'rejected-filter-btn') {
+        countJobs.innerText = `${rejectedList.length} of ${currentTotal}`;
+    }
+    else {
+        countJobs.innerText = currentTotal;
+    }
+
+    interviewCount.innerText = interviewList.length;
+    rejectedCount.innerText = rejectedList.length;
+}
+calculateCount(); //Function Call
